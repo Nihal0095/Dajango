30695cabc3f4a036b39e0c08eea2cf811c44d40c
Nihal0095 <97807811+Nihal0095@users.noreply.github.com>
2024-05-09 20:01:18 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,20 +1,24 @@
-from django.urls import path
-from . import views
-from django.conf.urls.static import static 
+"""
+URL configuration for ipl project.
 
-urlpatterns =[
-    path('',views.list_page,name='list'),
-    path('login/',views.user_login,name='login'),
-    path('signup/',views.user_signup,name='signup'),
-    path('logout/',views.user_logout,name='logout'),
-    path('<int:id>',views.user ,name='details'),
-    path('home/',views.home,name='home'),
-    path('product',views.product,name='product'),
-    path('rangelist/',views.student_range_list_view),
-    path('hello',views.hello,name='hello'),
-    path('search',views.search,name='search'),
-    path('productlist/',views.product_list,name='product_list'),
-    path('cart/',views.view_cart,name='view_cart'),
-    path('add/<int:product_id>',views.add_to_cart,name='add_to_cart'),
-    path('remove/<int:item_id>',views.remove_from_cart,name='remove_from_cart'),
-                       ]
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns=[
+    path('admin/',admin.site.urls),
+    path('',include('rcb.urls'))
+]
+
